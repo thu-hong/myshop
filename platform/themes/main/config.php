d91f17dae21e166a8dd2c405dc07ebfee0351b00
Mai Xuân Thưởng <thuong.mx@visualweber.com>
2021-05-09 21:59:30 +0700

    sass

--- platform/themes/main/config.php
+++ platform/themes/main/config.php
@@ -35,24 +35,23 @@ return [
         // Before event inherit from package config and the theme that call before,
         // you can use this event to set meta, breadcrumb template or anything
         // you want inheriting.
-        'before' => function($theme)
-        {
+        'before' => function ($theme) {
             // You can remove this line anytime.
         },
 
         // Listen on event before render a theme,
         // this event should call to assign some assets,
         // breadcrumb template.
-        'beforeRenderTheme' => function (Theme $theme)
-        {
+        'beforeRenderTheme' => function (Theme $theme) {
             $theme->asset()->add('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css'); //fontawesome
             $theme->asset()->add('fontawesome2', 'https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'); //fontawesome
             // $theme->asset()->container('after_header')->add('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css'); //bootstrap
             // $theme->asset()->container('after_header')->add('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css');
-           
-            
+
+
             $theme->asset()->container('after_header')->usePath()->add('myresponsee', 'templates/css/myresponsee.css');
             $theme->asset()->container('after_header')->usePath()->add('style1', 'templates/css/style.css');
+            $theme->asset()->container('after_header')->usePath()->add('common', 'css/common.css');
             $theme->asset()->container('after_header')->usePath()->add('carousel', 'templates/css/owl-carousel/owl.carousel.css');
             $theme->asset()->container('after_header')->usePath()->add('theme', 'templates/css/owl-carousel/owl.theme.css');
             $theme->asset()->container('after_header')->usePath()->add('responsive', 'templates/css/responsive.css');
@@ -61,9 +60,9 @@ return [
             // $theme->asset()->container('after_header')->usePath()->add('hover', 'templates/css/hover.css');
             // $theme->asset()->container('after_header')->usePath()->add('imagehover', 'templates/css/imagehover.css');
             // $theme->asset()->container('after_header')->usePath()->add('normalize', 'templates/css/normalize.css');
-           
+
             // $theme->asset()->container('after_header')->usePath()->add('common', 'css/common.css'); //css
-   
+
             // $theme->asset()->container('footer')->usePath()->add('jquery', 'templates/js/jquery.min.js'); //js
             $theme->asset()->container('footer')->usePath()->add('jquery-3.1.1', 'templates/js/jquery-3.1.1.min.js');
             $theme->asset()->container('footer')->usePath()->add('jquery-1.8.3', 'templates/js/jquery-1.8.3.min.js');
@@ -81,7 +80,7 @@ return [
             // <script type="text/javascript" src="js/jquery-ui.min.js"></script>
             // <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>
             // <script type="text/javascript" src="js/custom.js"></script>
-// $theme->asset()->container('footer')->add('jqueryboostrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js');
+            // $theme->asset()->container('footer')->add('jqueryboostrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js');
             // $theme->asset()->container('footer')->add('aos.js', 'https://unpkg.com/aos@next/dist/aos.js');
             // $theme->asset()->container('footer')->usePath()->add('common', 'js/common.js'); //js
 
@@ -96,8 +95,7 @@ return [
         // this should call to assign style, script for a layout.
         'beforeRenderLayout' => [
 
-            'default' => function ($theme)
-            {
+            'default' => function ($theme) {
                 // $theme->asset()->usePath()->add('ipad', 'css/layouts/ipad.css');
             }
         ]
--- platform/themes/main/partials/header.blade.php
+++ platform/themes/main/partials/header.blade.php
@@ -1,113 +1,107 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=1" name="viewport"/>
 
-        {!! Theme::header() !!}
-    </head>
-	<body class="size-1140" @if (class_exists('Language', false) && Language::getCurrentLocaleRTL()) dir="rtl" @endif>
-		<!-- HIDDEN PRODUCTS MENU START -->
-		<nav id="main-nav">
-			<ul>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Facial Wash</span></a></li>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Exflotiate Scrub</span></a></li>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Natural Skin Care</span></a></li>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Facial Toner</span></a></li>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Moisturizing Cream</span></a></li>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Anti Wrinkle Cream</span></a></li>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Sun Protection</span></a></li>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Hand Cream</span></a></li>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Hair Removing Cream</span></a></li>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Foot Cream</span></a></li>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Under Arm Protection</span></a></li>
-				<li><a href="products.html" class="hvr-sweep-to-bottom"><span>Slimming Cream</span></a></li>
-			</ul>
-			<a href="#0" class="hidden-menu-close">Close<span></span></a>
-		</nav>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=1"
+        name="viewport" />
 
-		<div id="page-main-content">
-			<!-- HEADER START -->
-			<header> 
-				<!-- top bar start -->
-				<div id="topbar-primary" class="background-primary">
-					<div class="line">
-						<!-- social icons -->
-						<div class="s-12 m-12 l-6">
-							<ul>
-							<li><a href="#"><i class="fa fa-instagram"></i></a></li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-							<li><a href="#"><i class="fa fa-youtube"></i></a></li>
-							<li class="hide-s"><a href="#"><i class="fa fa-pinterest"></i></a></li>
-							</ul>
-						</div>
-						<!-- contact details -->
-						<div class="l-6 hide-s hide-m">
-							<div class="right">
-								<ul class="topbar-menu">
-									<li><a href="#">info@website.com</a></li>
-									<li><a href="#">+971 123 456 789</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- top bar end -->
-				
-				<!-- navigation and logo -->
-				<nav class="background-dark">
-					<div class="line">
-						<!-- logo -->
-						<div class="s-12 l-3">
-							<div class="fullwidth">
-								<a href="index.html" class="logo"><img src="{{ Theme::asset()->url('img/logo.png') }}" alt=""></a>
-							</div>
-						</div>
-						
-						<!-- navigation -->
-						<div class="top-nav s-12 m-12 l-9">
-							<a class="nav-text"></a>
-							<ul class="right chevron">
-								<li><a class="hvr-sweep-to-bottom">Home</a>
-									<ul>
-										<li><a href="index.html" class="hvr-sweep-to-bottom">Home Page 1</a></li>
-										<li><a href="index-2.html" class="hvr-sweep-to-bottom">Home Page 2</a></li>
-									</ul>
-								</li>
-								<li><a class="hidden-menu-trigger hvr-sweep-to-bottom" href="#main-nav">Products</a></li>
-								<li><a class="hvr-sweep-to-bottom">Design One</a>
-									<ul>
-										<li><a href="about-us.html" class="hvr-sweep-to-bottom">About Us</a></li>
-										<li><a href="contact-us.html" class="hvr-sweep-to-bottom">Contact Us</a></li>
-										<li><a href="products.html" class="hvr-sweep-to-bottom">Products Grid</a></li>
-										<li><a href="product.html" class="hvr-sweep-to-bottom">Product Page</a></li>
-										<li><a href="cart.html" class="hvr-sweep-to-bottom">Cart</a></li>
-										<li><a href="blog.html" class="hvr-sweep-to-bottom">Blog</a></li>
-										<li><a href="blog-post.html" class="hvr-sweep-to-bottom">Blog Post</a></li>
-										<li><a href="page-not-found.html" class="hvr-sweep-to-bottom">Page Not Found</a></li>
-									</ul>
-								</li>
-								<li><a class="hvr-sweep-to-bottom">Design Two</a>
-									<ul>
-										<li><a href="about-us-2.html" class="hvr-sweep-to-bottom">About Us</a></li>
-										<li><a href="contact-us-2.html" class="hvr-sweep-to-bottom">Contact Us</a></li>
-										<li><a href="products-2.html" class="hvr-sweep-to-bottom">Products Grid</a></li>
-										<li><a href="product-2.html" class="hvr-sweep-to-bottom">Product Page</a></li>
-										<li><a href="cart-2.html" class="hvr-sweep-to-bottom">Cart</a></li>
-										<li><a href="blog-2.html" class="hvr-sweep-to-bottom">Blog</a></li>
-										<li><a href="blog-post-2.html" class="hvr-sweep-to-bottom">Blog Post</a></li>
-										<li><a href="page-not-found-2.html" class="hvr-sweep-to-bottom">Page Not Found</a></li>
-									</ul>
-								</li>
-								<li><a class="hvr-sweep-to-bottom" href="../light/index.html">Light Version</a></li>
-							</ul>
-						</div>
-						
-					</div>
-				</nav>
-			</header>
-			<!-- HEADER END -->
+    {!! Theme::header() !!}
+</head>
+
+<body class="size-1140" @if (class_exists('Language', false) && Language::getCurrentLocaleRTL()) dir="rtl" @endif>
+    <!-- HIDDEN PRODUCTS MENU START -->
+    <div id="page-main-content">
+        <!-- HEADER START -->
+        <header>
+            <!-- top bar start -->
+            <div id="topbar-primary" class="background-primary">
+                <div class="line">
+                    <!-- social icons -->
+                    <div class="s-12 m-12 l-6">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                            <li class="hide-s"><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                        </ul>
+                    </div>
+                    <!-- contact details -->
+                    <div class="l-6 hide-s hide-m line-right">
+                        <div class="right">
+                            <ul class="topbar-menu">
+                                <li class="topbar-menu-item">
+                                    <span class="number-cart">
+                                        0
+                                    </span>
+                                    <a href="#">
+                                        <i class="fas fa-shopping-cart"></i>
+                                    </a>
+                                </li>
+                                <li><a href="#">info@website.com</a></li>
+                                <li><a href="#">+971 123 456 789</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- top bar end -->
+
+            <!-- navigation and logo -->
+            <nav class="background-dark">
+                <div class="line">
+                    <!-- logo -->
+                    <div class="s-12 l-3">
+                        <div class="fullwidth">
+                            <a href="{{ route('public.index') }}" class="logo"><img
+                                    src="{{ Theme::asset()->url('img/logo.png') }}" alt=""></a>
+                        </div>
+                    </div>
+
+                    <!-- navigation -->
+                    <div class="top-nav s-12 m-12 l-9">
+                        <a class="nav-text"></a>
+                        <ul class="right chevron">
+                            <li><a class="hvr-sweep-to-bottom">Home</a>
+                                <ul>
+                                    <li><a href="{{ route('public.index') }}" class="hvr-sweep-to-bottom">Home Page
+                                        </a></li>
+                                </ul>
+                            </li>
+                            <li><a href="{{ route('public.get-product') }}" class="hvr-sweep-to-bottom">Product</a>
+                                <ul>
+                                    <li><a href="{{ route('public.get-product-detail') }}"
+                                            class="hvr-sweep-to-bottom">Product-Detail</a></li>
+                            </li>
+                        </ul>
+                        </li>
+                        <li><a class="hvr-sweep-to-bottom">Design One</a>
+                            <ul>
+                                <li><a href="{{ route('public.about-us') }}" class="hvr-sweep-to-bottom">About
+                                        Us</a></li>
+                                <li><a href="{{ route('public.get-contact') }}" class="hvr-sweep-to-bottom">Contact
+                                        Us</a></li>
+                                <li><a href="{{ route('public.get-product') }}" class="hvr-sweep-to-bottom">Products
+                                        Category</a></li>
+                                <li><a href="{{ route('public.get-product-detail') }}"
+                                        class="hvr-sweep-to-bottom">Product Detail</a></li>
+                                <li><a href="{{ route('public.get-cart') }}" class="hvr-sweep-to-bottom">Cart</a>
+                                </li>
+                                <li><a href="{{ route('public.blog') }}" class="hvr-sweep-to-bottom">Blog</a></li>
+                                <li><a href="{{ route('public.blog-post') }}" class="hvr-sweep-to-bottom">Blog
+                                        Post</a></li>
+                                <li><a href="{{ route('public.get-cart') }}" class="hvr-sweep-to-bottom">Page Not
+                                        Found</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </div>
+
+                </div>
+            </nav>
+        </header>
+        <!-- HEADER END -->
