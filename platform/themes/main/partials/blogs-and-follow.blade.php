24e86a8e0f7a5264eb6d9742ad7bb6b9b3ddc284
dautiep <dautiep123@gmail.com>
2021-05-09 17:55:07 +0700

    page homepage

---
+++ platform/themes/main/partials/blogs-and-follow.blade.php
@@ -0,0 +1,73 @@
+<!-- BLOG AND FOLLOW US START -->
+<div class="section"> 
+	<div class="line">
+		<div class="margin">
+			<!-- tutorial -->
+			<div class="s-12 m-12 l-4 margin-m-bottom-60">
+				<div class="fullwidth margin-bottom">
+					<a href="" class="image-hover-zoom">
+					    <img src="{{ Theme::asset()->url('img/tutorial.jpg') }}" alt="">
+					</a>
+				</div>
+				<div class="fullwidth">
+					<h3>Lovely tutorial</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempus quis eros ut aliquam.</p>
+			    </div>
+			</div>
+			
+			<!-- follow us -->
+			<div class="s-12 m-12 l-4 margin-m-bottom-60">
+				<div class="fullwidth margin-bottom">
+					<div class="s-4 m-4 l-4">
+					  <a href="">
+					      <i class="fa fa-facebook follow-facebook"></i>
+					  </a>
+					</div>
+					<div class="s-4 m-4 l-4">
+					  <a href="">
+					      <i class="fa fa-twitter follow-twitter"></i>
+					  </a>
+					</div>
+					<div class="s-4 m-4 l-4">
+					  <a href="">
+					      <i class="fa fa-pinterest follow-pinterest"></i>
+					  </a>
+					</div>
+					<div class="s-4 m-4 l-4">
+					  <a href="">
+					      <i class="fa fa-instagram follow-instagram"></i>
+					  </a>
+					</div>
+					<div class="s-4 m-4 l-4">
+					  <a href="">
+					      <i class="fa fa-youtube follow-youtube"></i>
+					  </a>
+					</div>
+					<div class="s-4 m-4 l-4">
+					  <a href="">
+					      <i class="fa fa-google-plus follow-gplus"></i>
+					  </a>
+					</div>
+				</div>
+				<div class="fullwidth">
+					<h3>Follow us</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempus quis eros ut aliquam.</p>
+			    </div>
+			</div>
+			
+			<!-- blog post -->
+			<div class="s-12 m-12 l-4">
+				<div class="fullwidth margin-bottom">
+				    <a href="" class="image-hover-zoom">
+					    <img src="{{ Theme::asset()->url('img/tutorial.jpg') }}" alt="">
+					</a>
+				</div>
+				<div class="fullwidth">
+					<h3>Read our blog</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempus quis eros ut aliquam.</p>
+			    </div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- BLOG AND FOLLOW US END -->
